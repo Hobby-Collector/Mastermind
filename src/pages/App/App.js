5b407bd54bceffe325b5f1be3ebf218fbaab5d28
jim-clark <email@jim-clark.com>
2020-01-16 14:53:10 -0800

    Step 8

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -184,7 +184,7 @@ class App extends Component {
     this.setState({user: null});
   }
 
-  handleSignup = () => {
+  handleSignupOrLogin = () => {
     this.setState({user: userService.getUser()});
   }
 
@@ -229,12 +229,13 @@ class App extends Component {
           <Route exact path='/signup' render={({ history }) => 
             <SignupPage
               history={history}
-              handleSignup={this.handleSignup}
+              handleSignupOrLogin={this.handleSignupOrLogin}
             />
           }/>
-          <Route exact path='/login' render={() => 
+          <Route exact path='/login' render={({ history }) => 
             <LoginPage
-              
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
             />
           }/>
           <Route exact path='/high-scores' render={() => 
